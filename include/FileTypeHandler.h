c50d8f3897d7c744ac5e12c030d6519a13979749
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-12 01:50:54 +0200

    add FileTypeHandler class

---
+++ include/FileTypeHandler.h
@@ -0,0 +1,63 @@
+#ifndef _FILETYPEHANDLER_H_
+#define _FILETYPEHANDLER_H_
+
+#include <vector>
+#include <string>
+#include <map>
+#include <memory>
+
+class ArcData;
+class Graph;
+
+class FileTypeHandler {
+ public:
+  FileTypeHandler(const std::string & _description, bool can_write);
+  virtual ~FileTypeHandler() { }
+  
+  virtual std::shared_ptr<Graph> openGraph(const char * filename) = 0;
+  
+  std::shared_ptr<Graph> openGraph(const std::string & filename) { return openGraph(filename.c_str()); }
+
+  virtual bool saveGraph(const Graph & graph, const std::string & filename) { return false; }
+  
+  const std::vector<std::string> & getExtensions() const { return extensions; }
+  const std::string & getDescription() const { return description; }
+  bool canWrite() const { return can_write; }
+
+ protected:
+  static int createNode(Graph & graph, std::map<std::string, int> & nodes, double x, double y, double z = 0, float nx = 0, float ny = 0, float nz = 0);
+  static std::pair<int, int> createNodesForArc(const ArcData & arc, Graph & graph, std::map<std::string, int> & nodes, bool rev = false);
+
+  void addExtension(const std::string & str) { extensions.push_back(str); }
+  
+ private:
+  std::string description;
+  std::vector<std::string> extensions;
+  bool can_write;
+};
+
+#if 0
+class FileTypeHandlerFactory {
+ public:
+ FileTypeHandlerFactory(bool _can_read, bool _can_write) : can_read(_can_read), can_write(_can_write) { }
+
+  bool canRead() const { return can_read; }
+  bool canWrite() const { return can_write; }
+
+ protected:
+  void canRead(bool t) { can_read = t; }
+  void canWrite(bool t) { can_write = t; }
+
+ private:
+  bool can_read, can_write;
+};
+#endif
+
+class DummyHandler : public FileTypeHandler {
+ public:
+ DummyHandler() : FileTypeHandler("", false) { }
+  
+  std::shared_ptr<Graph> openGraph(const char * filename) override { return std::shared_ptr<Graph>(0); }
+};
+
+#endif
---
+++ src/FileTypeHandler.cpp
@@ -0,0 +1,98 @@
+#include "FileTypeHandler.h"
+
+#include <Graph.h>
+#include <glm/glm.hpp>
+#include <sstream>
+
+using namespace std;
+
+FileTypeHandler::FileTypeHandler(const string & _description, bool _can_write) : description(_description), can_write(_can_write) {
+
+}
+
+#if 0
+glm::dvec2
+FileTypeHandler:compute2DPolygonCentroid(const Point2D* vertices, int vertexCount) {
+  glm::dvec2 centroid;
+  double signedArea = 0.0;
+  double x0 = 0.0; // Current vertex X
+  double y0 = 0.0; // Current vertex Y
+  double x1 = 0.0; // Next vertex X
+  double y1 = 0.0; // Next vertex Y
+  double a = 0.0;  // Partial signed area
+  
+  // For all vertices except last
+  int i = 0;
+  for (i = 0; i < vertexCount - 1; ++i) {
+    x0 = vertices[i].x;
+    y0 = vertices[i].y;
+    x1 = vertices[i+1].x;
+    y1 = vertices[i+1].y;
+    a = x0*y1 - x1*y0;
+    signedArea += a;
+    centroid.x += (x0 + x1)*a;
+    centroid.y += (y0 + y1)*a;
+  }
+  
+  // Do last vertex
+  x0 = vertices[i].x;
+  y0 = vertices[i].y;
+  x1 = vertices[0].x;
+  y1 = vertices[0].y;
+  a = x0*y1 - x1*y0;
+  signedArea += a;
+  centroid.x += (x0 + x1)*a;
+  centroid.y += (y0 + y1)*a;
+  
+  signedArea *= 0.5;
+  centroid.x /= (6.0*signedArea);
+  centroid.y /= (6.0*signedArea);
+  
+  return centroid;
+}
+#endif
+
+int
+FileTypeHandler::createNode(Graph & graph, map<string, int> & nodes, double x, double y, double z, float nx, float ny, float nz) {
+  ostringstream key;
+  key << x << "/" << y << '/' << z;
+  map<string, int>::iterator it = nodes.find(key.str());
+  if (it != nodes.end()) {
+    return it->second;
+  } else {
+    int node_id = nodes[key.str()] = graph.addNode();
+    graph.setPosition(node_id, glm::vec3(x, y, z));
+    graph.setNormal(node_id, glm::vec4(nx, ny, nz, 1.0f));
+    return node_id;  
+  }
+}
+
+pair<int, int>
+FileTypeHandler::createNodesForArc(const ArcData & arc, Graph & graph, map<string, int> & nodes, bool rev) {
+  assert(arc.data.size() >= 2);
+  const glm::dvec3 & v1 = arc.data.front(), v2 = arc.data.back();
+  ostringstream key1, key2;
+  key1 << v1.x << "/" << v1.y;
+  key2 << v2.x << "/" << v2.y;
+  int node1, node2;
+  map<string, int>::iterator it = nodes.find(key1.str());
+  if (it != nodes.end()) {
+    node1 = it->second;
+  } else {
+    nodes[key1.str()] = node1 = graph.addNode();
+    graph.setPosition(node1, glm::vec3(v1.x, v1.y, v1.z));
+  }
+  it = nodes.find(key2.str());
+  if (it != nodes.end()) {
+    node2 = it->second;
+  } else {
+    nodes[key2.str()] = node2 = graph.addNode();
+    graph.setPosition(node2, glm::vec3(v2.x, v2.y, v2.z));
+  }
+  assert(node1 >= 0 && node2 >= 0);
+  if (rev) {
+    return pair<int, int>(node1, node2);
+  } else {
+    return pair<int, int>(node2, node1);
+  }
+}
