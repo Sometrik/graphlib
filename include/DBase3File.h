0b39967f8cfae2dad47f88637b5a93aaea87f469
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-11 02:03:45 +0200

    remove shapefil.h dependency from DBase3File.h by adding wrapper for DBFHandle

--- include/DBase3File.h
+++ include/DBase3File.h
@@ -6,47 +6,26 @@
 #include <memory>
 #include <vector>
 
-#include <shapefil.h>
-
 namespace table {
+  class DBase3Handle;
+  
   class ColumnDBase3 : public Column {
   public:
-  ColumnDBase3(DBFHandle _dbf,
-	       int _column_index,
-	       int _num_rows,
-	       const std::string & _name);
+    ColumnDBase3(std::shared_ptr<DBase3Handle> _dbf,
+		 int _column_index,
+		 int _num_rows,
+		 const std::string & _name);
 
   std::shared_ptr<Column> copy() const override { return std::make_shared<ColumnDBase3>(*this); }
   std::shared_ptr<Column> create() const override { return std::shared_ptr<Column>(0); } // FIXME
   
     size_t size() const override { return num_rows; }
     
-    double getDouble(int i) const override {
-      if (i >= 0 && i < num_rows) {
-	return DBFReadIntegerAttribute(dbf, i, column_index);
-      } else {
-	return 0;
-      }
-    }
-    int getInt(int i) const override {
-      if (i >= 0 && i < num_rows) {
-	return DBFReadIntegerAttribute(dbf, i, column_index);
-      } else {
-	return 0;
-      }
-    }
-    long long getInt64(int i) const override {
-      if (i >= 0 && i < num_rows) {
-	return DBFReadIntegerAttribute(dbf, i, column_index);
-      } else {
-	return 0;
-      }
-    }
+    double getDouble(int i) const override;
+    int getInt(int i) const override;
+    long long getInt64(int i) const override;
     std::string getText(int i) const override;
-    
-    // const char * DBFReadLogicalAttribute( DBFHandle hDBF, int iShape, int iField );
-    // int DBFIsAttributeNULL( DBFHandle hDBF, int iShape, int iField );
-    
+        
     void setValue(int i, double v) override { }
     void setValue(int i, int v) override { }
     void setValue(int i, long long v) override { }
@@ -64,7 +43,7 @@ namespace table {
     }
     
   private:
-    DBFHandle dbf;
+    std::shared_ptr<DBase3Handle> dbf;
     int column_index;
     unsigned int num_rows;
   };
@@ -72,19 +51,18 @@ namespace table {
   class DBase3File {
   public:
     DBase3File(const std::string & filename);
-    ~DBase3File();
     
     unsigned int getRecordCount() const { return record_count; }
-    
     std::vector<std::shared_ptr<Column> > & getColumns() { return columns; }
     
   private:
     bool openDBF(const std::string & filename);
     
-    DBFHandle dbf;
+    std::shared_ptr<DBase3Handle> dbf;
     unsigned int record_count;
     std::vector<std::shared_ptr<Column> > columns;
   };
 };
 
 #endif
+
--- src/DBase3File.cpp
+++ src/DBase3File.cpp
@@ -9,44 +9,79 @@
 #include <cstdio>
 #include <cstring>
 #include <cassert>
+#include <shapefil.h>
 
 using namespace std;
 using namespace table;
 
-DBase3File::DBase3File(const string & filename)
- {
-  dbf = 0;
-  record_count = 0;
+class table::DBase3Handle {
+public:
+  DBase3Handle() { }
+  ~DBase3Handle() {
+    if (h) {
+      DBFClose(h);
+    }
+  }
 
+  void open(const std::string & fn) {
+    h = DBFOpen(fn.c_str(), "rb");  
+  }
+  
+  int readIntegerAttribute(int rec, int field) { return DBFReadIntegerAttribute(h, rec, field); }
+  double readDoubleAttribute(int rec, int field) { return DBFReadDoubleAttribute(h, rec, field); }
+  std::string readStringAttribute(int rec, int field) {
+    const char * tmp = DBFReadStringAttribute(h, rec, field);
+    if (tmp) {
+#ifdef _WIN32
+      string output;
+      while (!tmp) {
+	utf8::append((unsigned char)*tmp, back_inserter(output));
+	tmp++;
+      }
+      return output;
+#else
+      IConv iconv("ISO8859-1", "UTF-8");
+      string tmp2;
+      if (iconv.convert(tmp, tmp2)) {
+      return tmp2;
+      }
+#endif
+    }
+    return "";
+  }
+  bool readBoolAttribute(int rec, int field) { return DBFReadLogicalAttribute(h, rec, field); }
+  bool isNull(int rec, int field) { return DBFIsAttributeNULL(h, rec, field); }
+  unsigned int getRecordCount() { return DBFGetRecordCount(h); }
+  unsigned int getFieldCount() { return DBFGetFieldCount(h); }
+  string getFieldName(int field) {
+    char fieldname[255];
+    DBFFieldType type = DBFGetFieldInfo(h, field, fieldname, 0, 0);
+    return fieldname;
+  }
+  
+private:
+  DBFHandle h = 0;
+};
+
+DBase3File::DBase3File(const string & filename) {
+  record_count = 0;
   openDBF(filename);
 }
 
-DBase3File::~DBase3File() {
-  if (dbf) DBFClose(dbf);
-}
-
 bool
-DBase3File::openDBF(const string & filename) { 
-  dbf = DBFOpen(filename.c_str(), "rb");
-  if (!dbf) {
-    fprintf(stderr, "failed to open dbf file\n");
-    return false;
-  }
-  unsigned int field_count = DBFGetFieldCount(dbf);
-  record_count = DBFGetRecordCount(dbf);
-  
-  char fieldname[255];
+DBase3File::openDBF(const string & filename) {
+  dbf = std::make_shared<DBase3Handle>();
+  dbf->open(filename);
+  record_count = dbf->getRecordCount();
+  unsigned int field_count = dbf->getFieldCount();
   for (unsigned int i = 0; i < field_count; i++) {
-    DBFFieldType type = DBFGetFieldInfo(dbf, i, fieldname, 0, 0);
-    ColumnDBase3 * col = new ColumnDBase3(dbf, i, record_count, fieldname);
-    columns.push_back(std::shared_ptr<Column>(col));
-    // addField(fieldname);
+    string name = dbf->getFieldName(i);
+    columns.push_back(std::make_shared<ColumnDBase3>(dbf, i, record_count, name));
   }
-  
   return true;
 }
 
-ColumnDBase3::ColumnDBase3(DBFHandle _dbf,
+ColumnDBase3::ColumnDBase3(const std::shared_ptr<DBase3Handle> _dbf,
 			   int _column_index,
 			   int _num_rows,
 			   const std::string & _name)
@@ -58,25 +93,38 @@ ColumnDBase3::ColumnDBase3(DBFHandle _dbf,
 
 }
 
+long long
+ColumnDBase3::getInt64(int i) const {
+  if (i >= 0 && i < num_rows) {
+    return dbf->readIntegerAttribute(i, column_index);
+  } else {
+    return 0;
+  }
+}
+    
 std::string
 ColumnDBase3::getText(int i) const {
-  assert(i >= 0 && i < num_rows);
-  const char * tmp = DBFReadStringAttribute(dbf, i, column_index);
-  if (tmp) {
-#ifdef _WIN32
-    string output;
-    while (!tmp) {
-      utf8::append((unsigned char)*tmp, back_inserter(output));
-      tmp++;
-    }
-    return output;
-#else
-    IConv iconv("ISO8859-1", "UTF-8");
-    string tmp2;
-    if (iconv.convert(tmp, tmp2)) {
-      return tmp2;
-    }
-#endif
+  if (i >= 0 && i < num_rows) {
+    return dbf->readStringAttribute(i, column_index);
+  } else {
+    return "";
   }
-  return "";
+}
+
+double
+ColumnDBase3::getDouble(int i) const {
+  if (i >= 0 && i < num_rows) {
+    return dbf->readIntegerAttribute(i, column_index);
+  } else {
+    return 0;
+  }
+}
+
+int
+ColumnDBase3::getInt(int i) const {
+  if (i >= 0 && i < num_rows) {
+    return dbf->readIntegerAttribute(i, column_index);
+  } else {
+    return 0;
+  }
 }
