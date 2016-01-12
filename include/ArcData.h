a2a608662c60493310306b471cf4a9df6045bfd4
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-12 02:17:01 +0200

    add ArcData.h

---
+++ include/ArcData.h
@@ -0,0 +1,17 @@
+#ifndef _ARCDATA_H_
+#define _ARCDATA_H_
+
+#include <vector>
+#include <glm/glm.hpp>
+
+class ArcData {
+ public:
+  ArcData() { }
+
+  size_t size() const { return data.size(); }
+  bool empty() const { return data.empty(); }
+  
+  std::vector<glm::dvec3> data;
+};
+
+#endif
