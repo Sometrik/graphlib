b33f380d63469099c241bbd5bf69a6ab83f744a4
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-16 17:00:53 +0200

    add PlanarGraph

---
+++ include/PlanarGraph.h
@@ -0,0 +1,122 @@
+#ifndef _PLANARGRAPH_H_
+#define _PLANARGRAPH_H_
+
+#include <Graph.h>
+
+struct planar_edge_data_s : public edge_data_s {
+ planar_edge_data_s() { }
+ planar_edge_data_s(float _weight, int _tail, int _head, int _next_node_edge, int _face, int _next_face_edge, int _arc)
+   : edge_data_s(_weight, _tail, _head, _next_node_edge, _face, _next_face_edge, _arc) { }
+
+ int pair_edge = -1;
+};
+
+class PlanarGraph : public Graph {
+ public:
+  PlanarGraph(int _id = 0);
+  PlanarGraph(const PlanarGraph & other);
+  
+  bool checkConsistency() const override;
+
+  std::shared_ptr<Graph> createSimilar() const override;
+  Graph * copy() const override { return new PlanarGraph(*this); }
+  
+  void addUniversalRegion() override;
+
+  void setEdgeFace(int edge, int face) override {
+    assert(edge_attributes[edge].face <= 0); // ??? universal?
+    edge_attributes[edge].face = face;
+    if (face != -1) {
+      edge_attributes[edge].next_face_edge = getFaceFirstEdge(face);
+      face_attributes[face].first_edge = edge;
+    }
+  }
+
+  int addEdge(int n1, int n2, int face = -1, float weight = 1.0f, int arc = 0) override {
+    int edge = (int)edge_attributes.size();
+    int next_node_edge = -1;
+    if (n1 != -1) {
+      next_node_edge = getNodeFirstEdge(n1);
+      node_geometry2[n1].first_edge = edge;
+      node_geometry2[n1].outdegree++;
+      total_outdegree++;
+      if (getNodeSizeMethod().definedForSource()) {
+	updateNodeSize(n1);
+      }
+    }
+    if (n2 != -1 && n1 != n2) {
+      // next_head_edge = getNodeFirstEdge(n2);
+      // node_geometry2[n2].first_edge = edge;
+      node_geometry2[n2].indegree++;
+      total_indegree++;
+      if (getNodeSizeMethod().definedForTarget()) {
+	updateNodeSize(n2);
+      }  
+    }
+
+    edge_attributes.push_back(planar_edge_data_s( weight, n1, n2, next_node_edge, -1, -1, arc ));
+    edges.addRow();
+    total_edge_weight += fabsf(weight);
+
+    setEdgeFace(edge, face);
+
+    return edge;
+  }
+
+  int addRegion() override {
+    int region_id = (int)region_attributes.size();
+    region_attributes.push_back({ { 255, 255, 255, 255 }, "", Rect2d(), -1 });
+    regions.addRow();
+    return region_id;
+  }
+
+  int getRegionFirstFace(int region) const override {
+    return region_attributes[region].first_face;
+  }
+
+  int getRegionNextFace(int region, int face) const override {
+    return face_attributes[face].next_face_in_region;
+  }
+
+  int getNextFaceEdge(int edge) const override {
+    return edge_attributes[edge].next_face_edge;
+  }
+
+  std::set<int> getAdjacentRegions() const override;
+
+  void updateMBR(int edge) override;
+
+  void mapFacesToNodes(Graph & target) override;
+  void mapRegionsToNodes(Graph & target) override;
+  void calculateRegionAreas() override;
+  void colorizeRegions() override;
+
+  int findContainingRegion(const glm::dvec3 & v) const;
+  void spatialAggregation(const Graph & other) override;
+
+  void clear() override {
+    Graph::clear();
+    edge_attributes.clear();
+  }
+  
+ protected:
+  edge_data_s & getEdgeAttributes(int i) override { return edge_attributes[i]; }
+  const edge_data_s & getEdgeAttributes(int i) const override { return edge_attributes[i]; }
+  EdgeIterator begin_edges() override { return EdgeIterator(&(edge_attributes.front()), sizeof(planar_edge_data_s)); }
+  EdgeIterator end_edges() override {
+    EdgeIterator it(&(edge_attributes.back()), sizeof(planar_edge_data_s));
+    ++it;
+    return it;
+  }
+  ConstEdgeIterator begin_edges() const override { return ConstEdgeIterator(&(edge_attributes.front()), sizeof(planar_edge_data_s)); }
+  ConstEdgeIterator end_edges() const override {
+    ConstEdgeIterator it(&(edge_attributes.back()), sizeof(planar_edge_data_s));
+    ++it;
+    return it;
+  }
+  
+ private:
+  std::vector<planar_edge_data_s> edge_attributes;
+};
+
+#endif
---
+++ src/PlanarGraph.cpp
@@ -0,0 +1,358 @@
+#include "PlanarGraph.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace table;
+
+PlanarGraph::PlanarGraph(int _id) : Graph(2, _id) {
+  
+}
+
+PlanarGraph::PlanarGraph(const PlanarGraph & other)
+  : Graph(other), edge_attributes(other.edge_attributes) {
+
+}
+
+std::shared_ptr<Graph>
+PlanarGraph::createSimilar() const {
+  std::shared_ptr<Graph> graph(new PlanarGraph(getId()));
+  graph->setLocationGraphValid(false);
+  graph->setAlpha3(getAlpha2());
+  graph->setTemporal(isTemporal());
+  graph->setPersonality(getPersonality());
+  graph->setHasTextures(hasTextures());
+  graph->setNodeSizeMethod(getNodeSizeMethod());
+  graph->setClusterVisibility(getClusterVisibility());
+  graph->setNodeVisibility(getNodeVisibility());
+  graph->setEdgeVisibility(getEdgeVisibility());
+  graph->setRegionVisibility(getRegionVisibility());
+  graph->setLabelVisibility(getLabelVisibility());
+
+  for (auto it = getNodeData().getColumns().begin(); it != getNodeData().getColumns().end(); it++) {
+    if (it->first != "posts") {
+      graph->getNodeData().addColumn(it->second->create());
+    }
+  }
+
+  return graph;
+}
+
+bool
+PlanarGraph::checkConsistency() const {
+  return getNodeCount() - getEdgeCount() + getFaceCount() == 2;
+}
+
+void
+PlanarGraph::mapFacesToNodes(Graph & target) {
+#if 0
+  assert(!target.getNodeCount() && !target.getEdgeCount() && !target.getFaceCount());
+  for (unsigned int i = 0; i < getFaceCount(); i++) {
+    int node_id = target.addNode();
+    int region = getFaceRegion(i);
+    target.setNodeColor2(i, getRegionColor(region));
+  }
+  unsigned int added_edge_count = 0;
+  bool has_arcs = hasArcData();
+  for (unsigned int i = 0; i < getFaceCount(); i++) {
+    int edge = getFaceFirstEdge(i);
+    glm::vec3 center;
+    unsigned int n = 0;
+    while (edge != -1) {
+      if (has_arcs && 0) {
+
+      } else {
+	pair<int, int> ed = getFaceEdgeSourceNodeAndDirection(i, edge);
+	center += getPosition(ed.first);
+	n++;
+      }
+      int other_face = getEdgeTargetFace(i, edge);
+      if (other_face == i) {
+	cerr << "skipping self edge\n";
+      } else if (other_face != -1) {
+	target.addEdge(i, other_face);
+	added_edge_count++;
+      }
+      edge = getFaceNextEdge(i, edge);
+    }
+    if (!n) {
+      cerr << "graph has no nodes\n";
+    } else {
+      center.x /= n;
+      center.y /= n;
+      center.z /= n;
+      target.setPosition(i, center);
+    }
+  }
+
+  cerr << "added edges " << added_edge_count << endl;
+
+  map<string, std::shared_ptr<Column> > & columns = getFaceData().getColumns();
+  for (map<string, std::shared_ptr<Column> >::iterator it = columns.begin(); it != columns.end(); it++) {
+    if (it->first[0] != '_') {
+      cerr << "adding column " << it->first << endl;
+      target.getNodeData().addColumn(it->second);
+    }
+  }
+#endif
+}
+
+void
+PlanarGraph::mapRegionsToNodes(Graph & target) {
+#if 0
+  assert(!target.getNodeCount() && !target.getEdgeCount() && !target.getFaceCount());
+  for (unsigned int i = 0; i < getFaceCount(); i++) {
+    int node_id = target.addNode();
+    int region = getFaceRegion(i);
+    target.setNodeColor2(i, getRegionColor(region));
+  }
+  unsigned int added_edge_count = 0;
+  bool has_arcs = hasArcData();
+  for (unsigned int i = 0; i < getFaceCount(); i++) {
+    int edge = getFaceFirstEdge(i);
+    glm::vec3 center;
+    unsigned int n = 0;
+    while (edge != -1) {
+      if (has_arcs && 0) {
+
+      } else {
+	pair<int, int> ed = getFaceEdgeSourceNodeAndDirection(i, edge);
+	center += getPosition(ed.first);
+	n++;
+      }
+      int other_face = getEdgeTargetFace(i, edge);
+      if (other_face == i) {
+	cerr << "skipping self edge\n";
+      } else if (other_face != -1) {
+	target.addEdge(i, other_face);
+	added_edge_count++;
+      }
+      edge = getFaceNextEdge(i, edge);
+    }
+    if (!n) {
+      cerr << "graph has no nodes\n";
+    } else {
+      center.x /= n;
+      center.y /= n;
+      center.z /= n;
+      target.setPosition(i, center);
+    }
+  }
+
+  cerr << "added edges " << added_edge_count << endl;
+
+  map<string, std::shared_ptr<Column> > & columns = getFaceData().getColumns();
+  for (map<string, std::shared_ptr<Column> >::iterator it = columns.begin(); it != columns.end(); it++) {
+    if (it->first[0] != '_') {
+      cerr << "adding column " << it->first << endl;
+      target.getNodeData().addColumn(it->second);
+    }
+  }
+#endif
+}
+
+void
+PlanarGraph::calculateRegionAreas() {
+#if 0
+  Column & areas = regions.addDoubleColumn("Area");
+  ColumnArc & g = dynamic_cast<ColumnArc &>(edges["_geometry"]);
+
+  bool has_arcs = hasArcData();
+  assert(has_arcs);
+
+  for (unsigned int region = 0; region < getRegionCount(); region++) {
+    double area = 0;
+    int face = getRegionFirstFace(region);
+    while (face != -1) {
+      int edge = getFaceFirstEdge(face);
+      while (edge != -1) {
+	pair<int, int> ed = getFaceEdgeSourceNodeAndDirection(face, edge);
+	if (has_arcs) {
+	  const ArcData2D & arc = g.getArc(edge);
+	  for (unsigned int k = 0; k + 1 < arc.size(); k++) {
+	    const glm::dvec3 & v1 = arc.geometry[ed.second == -1 ? arc.size() - 1 - k : k];
+	    const glm::dvec3 & v2 = arc.geometry[ed.second == -1 ? arc.size() - 1 - (k + 1) : k + 1];
+	    area -= v1.x * v2.y - v1.y * v2.x;
+	  }
+	} else {
+	  assert(0);
+	}
+	edge = getFaceNextEdge(face, edge);
+      }
+      face = getRegionNextFace(region, face);
+    }
+    areas.setValue(region, area);
+  }
+#endif
+}
+
+void
+PlanarGraph::colorizeRegions() {
+  vector<glm::vec3> colors;
+  colors.push_back(glm::vec3(150.0f / 255.0f, 180.0f / 255.0f, 0.0f));
+  colors.push_back(glm::vec3(245.0f / 255.0f, 90.0f / 255.0f, 40.0f / 255.0f));
+  colors.push_back(glm::vec3(0.0f / 255.0f, 180.0f / 255.0f, 200.0f / 255.0f));
+  colors.push_back(glm::vec3(110.0f / 255.0f, 80.0f / 255.0f, 150.0f / 255.0f));
+  colors.push_back(glm::vec3(1.0f, 180.0f / 255.0f, 0.0f));
+  colors.push_back(glm::vec3(230.0f / 255.0f, 0.0f, 130.0f / 255.0f));
+  colors.push_back(glm::vec3(70.0f / 255.0f, 70.0f / 255.0f, 50.0f / 255.0f));
+  
+  vector<int> region_colors(getRegionCount(), 0);
+  assert(region_colors.size() == getRegionCount());
+
+  for (unsigned int i = 0; i < getRegionCount(); i++) {
+#if 0
+    int edge = getRegionFirstEdge(i);
+    set<int> available_colors;
+    for (unsigned int j = 0; j < colors.size(); j++) {
+      available_colors.insert(j);
+    }
+    while (edge != -1) {
+      int other_face = getEdgeTargetFace(i, edge);
+      if (other_face >= 0 && other_face < i) {
+	available_colors.erase(face_colors[i]);
+      }
+      edge = getFaceNextEdge(i, edge);
+    }
+    set<int>::iterator it = available_colors.begin();
+    assert(it != available_colors.end());
+    face_colors[i] = *it;
+    face_attributes[i].color = colors[*it];
+#endif
+  }
+}
+
+void
+PlanarGraph::updateMBR(int edge) {
+  if (hasArcData()) {
+#if 0
+    ColumnArc & g = dynamic_cast<ColumnArc&>(getEdgeData()["_geometry"]);
+    const ArcData2D & arc = g.getArc(edge);
+    
+    int left_face = edges["_leftFace"].getInt(edge);
+    int right_face = edges["_rightFace"].getInt(edge);
+    if (left_face != -1) {
+      Rect2d & face_mbr = face_attributes[left_face].mbr;
+      Rect2d & region_mbr = region_attributes[getFaceRegion(left_face)].mbr;
+      for (vector<glm::dvec3>::const_iterator it = arc.geometry.begin(); it != arc.geometry.end(); it++) {
+	face_mbr.growToContain(glm::vec3(it->x, it->y, it->z));
+	region_mbr.growToContain(glm::vec3(it->x, it->y, it->z));
+      }
+    }
+    if (right_face != -1) {
+      Rect2d & face_mbr = face_attributes[right_face].mbr;
+      Rect2d & region_mbr = region_attributes[getFaceRegion(right_face)].mbr;
+      for (vector<glm::dvec3>::const_iterator it = arc.geometry.begin(); it != arc.geometry.end(); it++) {
+	face_mbr.growToContain(glm::vec3(it->x, it->y, it->z));
+	region_mbr.growToContain(glm::vec3(it->x, it->y, it->z));
+      }
+    }
+#endif
+  }
+}
+
+static inline double determinant(double x1, double y1, double x2, double y2) {
+  return x1 * y2 - x2 * y1;
+}
+
+/**
+ * returns vector cross product of vectors p1p2 and p1p3 using Cramer's rule
+ */
+static inline double crossProduct(const glm::dvec3 & p1, const glm::dvec3 & p2, const glm::dvec3 & p3) {
+  double det_p2p3 = determinant(p2.x, p2.y, p3.x, p3.y);
+  double det_p1p3 = determinant(p1.x, p1.y, p3.x, p3.y);
+  double det_p1p2 = determinant(p1.x, p1.y, p2.x, p2.y);
+  return det_p2p3 - det_p1p3 + det_p1p2;
+}
+
+int
+PlanarGraph::findContainingRegion(const glm::dvec3 & point) const {
+#if 0
+  for (unsigned int i = 0; i < getFaceCount(); i++) {
+    if (1 || face_attributes[i].mbr.contains(point.x, point.y)) {
+      list<pair<int, int> > edges;
+      int edge = getFaceFirstEdge(i);
+      while (edge != -1) {
+	pair<int, int> ed = getFaceEdgeSourceNodeAndDirection(i, edge);
+	assert(ed.first != -1);
+	edges.push_front(pair<int, int>(edge, ed.second));
+	edge = getFaceNextEdge(i, edge);
+	assert(edge >= -1 && edge < (int)getEdgeCount());
+      }
+      assert(!edges.empty());
+    
+      int wn = 0; // the winding number counter
+  
+      const ColumnArc & g = dynamic_cast<const ColumnArc &>(getEdgeData()["_geometry"]);
+      glm::dvec3 v1;
+      for (list<pair<int, int> >::iterator it = edges.begin(); it != edges.end(); it++) {
+	const ArcData2D & arc = g.getArc(it->first);
+	for (unsigned int i = 0; i < arc.size(); i++) {
+	  const glm::dvec3 & v2 = arc.geometry[it->second == -1 ? arc.size() - 1 - i : i];
+	  if (!(v1.x == 0 && v1.y == 0 && v1.z == 0)) {
+	    if (v1.y <= point.y) { // start y <= P.y
+	      if (v2.y > point.y) { // an upward crossing
+		if (crossProduct(v1, v2, point) > 0) {
+		  // point left of edge
+		  wn++; // have a valid up intersect
+		}
+	      }
+	    } else { // start y > P.y (no test needed)
+	      if (v2.y <= point.y) { // a downward crossing
+		if (crossProduct(v1, v2, point) < 0) {
+		  // point right of edge
+		  wn--; // have a valid down intersect
+		}
+	      }
+	    }
+	  }
+	  v1 = v2;
+	}
+      }
+      if (wn != 0) {
+	return getFaceRegion(i);
+      }
+    }
+  }
+#endif
+  return -1;
+}
+
+void
+PlanarGraph::spatialAggregation(const Graph & other) {
+  const map<string, std::shared_ptr<Column> > & columns = other.getNodeData().getColumns();
+  for (map<string, std::shared_ptr<Column> >::const_iterator it = columns.begin(); it != columns.end(); it++) {
+    if (it->first == "COUNT" || it->first == "LIKES") {
+      cerr << "adding column " << it->first << endl;
+      getRegionData().addIntColumn(it->first.c_str());
+    }
+  }
+
+  for (unsigned int i = 0; i < other.getNodeCount(); i++) {
+    const glm::vec3 & v = other.getPosition(i);
+    int region = findContainingRegion(glm::dvec3(v.x, v.y, v.z));
+    if (region != -1) {
+      for (map<string, std::shared_ptr<Column> >::const_iterator it = columns.begin(); it != columns.end(); it++) {
+	const string & n = it->first;
+	if (n == "COUNT" || n == "LIKES") {
+	  Column & col = getRegionData()[n];
+	  col.setValue(region, col.getInt(region) + it->second->getInt(i));
+	}
+      }
+    }
+  }
+}
+
+set<int>
+PlanarGraph::getAdjacentRegions() const {
+  set<int> r;
+
+  return r;
+}
+
+void
+PlanarGraph::addUniversalRegion() {
+  int region = addRegion();
+  int face = addFace(region);
+  region_attributes[region].label = "UNIVERSAL";  
+}
