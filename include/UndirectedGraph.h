5d7cecac8f5597f8a519657a51c493efaec1f36f
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-16 17:01:58 +0200

    add UndirectedGraph

---
+++ include/UndirectedGraph.h
@@ -0,0 +1,87 @@
+#ifndef _UNDIRECTEDGRAPH_H_
+#define _UNDIRECTEDGRAPH_H_
+
+#include <Graph.h>
+
+class UndirectedGraph : public Graph {
+ public:
+  UndirectedGraph(int _id = 0);
+  UndirectedGraph(const UndirectedGraph & other);
+   
+  std::shared_ptr<Graph> createSimilar() const override;
+  Graph * copy() const override { return new UndirectedGraph(*this); }
+    
+  int addUndirectedEdge(int n1, int n2) {
+    int hyperedge_id = addFace(-1);
+    addEdge(n1, n2, hyperedge_id);
+    addEdge(n2, n1, hyperedge_id);
+    return hyperedge_id;
+  }
+  
+  int addEdge(int n1, int n2, int face = -1, float weight = 1.0f, int arc = 0) override {
+    assert(n1 != -1 || n2 != -1);
+    // assert(n1 != n2);
+    int edge = (int)edge_attributes.size();
+
+    int next_face_edge = -1;
+    if (face != -1) next_face_edge = getFaceFirstEdge(face);
+
+    int next_node_edge = -1;
+    if (n1 != -1) {
+      int prev_first_edge = getNodeFirstEdge(n1);
+      assert(edge != prev_first_edge);
+      node_geometry2[n1].first_edge = edge;
+      node_geometry2[n1].outdegree += 1; // weight;
+      total_outdegree += 1; // weight;
+      next_node_edge = prev_first_edge;
+
+      if (getNodeSizeMethod().definedForSource()) {
+	updateNodeSize(n1);
+      }
+    }
+    if (n2 != -1) { // && n1 != n2) {
+      // int prev_first_edge = getNodeFirstEdge(n2);
+      // assert(edge != prev_first_edge);
+      // node_geometry2[n2].first_edge = edge;
+      node_geometry2[n2].indegree += 1; // weight;
+      total_indegree += 1; // weight;
+      // next_head_edge = prev_first_edge;
+
+      if (getNodeSizeMethod().definedForTarget()) {
+	updateNodeSize(n2);
+      }      
+    }
+
+    edge_attributes.push_back(edge_data_s( weight, n1, n2, next_node_edge, face, next_face_edge, arc ));
+    edges.addRow();
+    total_edge_weight += fabsf(weight);
+
+    incVersion();
+    return edge;
+  }
+  
+  void clear() override {
+    Graph::clear();
+    edge_attributes.clear();
+  }  
+  
+  edge_data_s & getEdgeAttributes(int i) override { return edge_attributes[i]; }
+  const edge_data_s & getEdgeAttributes(int i) const override { return edge_attributes[i]; }
+  EdgeIterator begin_edges() override { return EdgeIterator(&(edge_attributes.front()), sizeof(edge_data_s)); }
+  EdgeIterator end_edges() override { 
+    EdgeIterator it(&(edge_attributes.back()), sizeof(edge_data_s));
+    ++it;
+    return it;
+  }
+  ConstEdgeIterator begin_edges() const override { return ConstEdgeIterator(&(edge_attributes.front()), sizeof(edge_data_s)); }
+  ConstEdgeIterator end_edges() const override { 
+    ConstEdgeIterator it(&(edge_attributes.back()), sizeof(edge_data_s));
+    ++it;
+    return it;
+  }
+
+ private:
+  std::vector<edge_data_s> edge_attributes;
+};
+
+#endif
---
+++ src/UndirectedGraph.cpp
@@ -0,0 +1,37 @@
+#include "UndirectedGraph.h"
+
+using namespace std;
+using namespace table;
+
+UndirectedGraph::UndirectedGraph(int _id) : Graph(1, _id) {
+  setNodeSizeMethod(SizeMethod::SIZE_FROM_DEGREE);  
+}
+
+UndirectedGraph::UndirectedGraph(const UndirectedGraph & other)
+  : Graph(other), edge_attributes(other.edge_attributes) {
+  
+}
+  
+std::shared_ptr<Graph>
+UndirectedGraph::createSimilar() const {
+  std::shared_ptr<Graph> graph(new UndirectedGraph(getId()));
+  graph->setLocationGraphValid(false);
+  graph->setAlpha3(getAlpha2());
+  graph->setTemporal(isTemporal());
+  graph->setPersonality(getPersonality());
+  graph->setHasTextures(hasTextures());
+  graph->setNodeSizeMethod(getNodeSizeMethod());
+  graph->setClusterVisibility(getClusterVisibility());
+  graph->setNodeVisibility(getNodeVisibility());
+  graph->setEdgeVisibility(getEdgeVisibility());
+  graph->setRegionVisibility(getRegionVisibility());
+  graph->setLabelVisibility(getLabelVisibility());
+
+  for (auto it = getNodeData().getColumns().begin(); it != getNodeData().getColumns().end(); it++) {
+    if (it->first != "posts") {
+      graph->getNodeData().addColumn(it->second->create());
+    }
+  }
+
+  return graph;
+}
