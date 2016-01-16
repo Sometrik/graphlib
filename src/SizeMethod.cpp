7314a9962679f1969518f8d524c202d33174fc69
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-16 02:10:50 +0200

    add SizeMethod.cpp

---
+++ src/SizeMethod.cpp
@@ -0,0 +1,43 @@
+#include "SizeMethod.h"
+
+#include <Graph.h>
+
+using namespace std;
+
+float
+SizeMethod::calculateSize(const node_secondary_data_s & data, double total_indegree, double total_outdegree, size_t node_count) const {
+  switch (method) {
+  case SIZE_FROM_DEGREE:
+    {
+      float degree = data.indegree + data.outdegree;      
+      float a;
+      if (degree) {
+	double avg_degree = float(total_indegree + total_outdegree) / node_count;
+	a = degree / avg_degree;
+      } else { // if this degree is zero, total degree might also be
+	a = 0;
+      }
+      return 9.0f + 6.0f * log(1.0 + a) / log(3.0);
+    }
+  case SIZE_FROM_INDEGREE:
+    {
+      float degree = data.indegree;
+      float a;
+      if (degree) {
+	float avg_degree = float(total_indegree / node_count);
+	a = degree / avg_degree;
+      } else {
+	a = 0;
+      }
+      return 9.0f + 6.0f * log(1.0 + a) / log(1.4);
+    }
+  case SIZE_FROM_NODE_COUNT:
+    {
+      auto & subgraph = data.nested_graph;
+      float a = subgraph.get() ? subgraph->getNodeCount() : 0;
+      return 9.0f + 6.0f * log(1.0 + a) / log(1.4);
+    }
+  default:
+    return 1.0f;
+  }
+}
