a4a7d96bda23b8cf6903e6be34bd9fbdb1cef24b
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-22 03:05:12 +0200

    set node parameters to NodeArray

--- src/PointCloud.cpp
+++ src/PointCloud.cpp
@@ -16,16 +16,16 @@ std::shared_ptr<Graph>
 PointCloud::createSimilar() const {
   std::shared_ptr<Graph> graph(new PointCloud(getId()));
   graph->setLocationGraphValid(false);
-  graph->setAlpha3(getAlpha2());
   graph->setTemporal(isTemporal());
   graph->setPersonality(getPersonality());
   graph->setHasTextures(hasTextures());
-  graph->setNodeSizeMethod(getNodeSizeMethod());
   graph->setClusterVisibility(getClusterVisibility());
   graph->setNodeVisibility(getNodeVisibility());
   graph->setEdgeVisibility(getEdgeVisibility());
   graph->setRegionVisibility(getRegionVisibility());
   graph->setLabelVisibility(getLabelVisibility());
+  graph->getNodeArray().setNodeSizeMethod(getNodeArray().getNodeSizeMethod());
+  graph->getNodeArray().setAlpha3(getNodeArray().getAlpha2());
 
   for (auto it = getNodeData().getColumns().begin(); it != getNodeData().getColumns().end(); it++) {
     if (it->first != "posts") {
