4348d3e236d7fb69dfb868ad16e564660d022627
Mikael Rekola <mikael.rekola@gmail.com>
2016-01-29 07:53:25 +0200

    add updateData method

--- include/DirectedGraph.h
+++ include/DirectedGraph.h
@@ -3,6 +3,9 @@
 
 #include <Graph.h>
 
+#include <unordered_set>
+#include <unordered_map>
+
 class DirectedGraph : public Graph {
  public:
   DirectedGraph(int _id = 0);
@@ -11,7 +14,10 @@ class DirectedGraph : public Graph {
   std::shared_ptr<Graph> createSimilar() const override;
   Graph * copy() const override { return new DirectedGraph(*this); }
   bool isDirected() const override { return true; }
-  
+  bool hasPosition() const override { return current_pos != -1; }
+
+  bool updateData(time_t start_time, time_t end_time, float start_sentiment, float end_sentiment, Graph & source_graph, RawStatistics & stats, bool is_first_level, Graph * base_graph) override;
+
   int addEdge(int n1, int n2, int face = -1, float weight = 1.0f, int arc = 0) override {
     assert(n1 != -1 && n2 != -1);
     int edge = (int)edge_attributes.size();
@@ -62,9 +68,24 @@ class DirectedGraph : public Graph {
     ++it;
     return it;
   }
+
+  void reset() override {
+    seen_nodes.clear();
+    seen_edges.clear();
+    current_pos = -1;
+    num_links = 0;
+    num_hashtags = 0;
+    min_time = 0;
+    max_time = 0;
+  }
   
  private:
   std::vector<edge_data_s> edge_attributes;
+  std::unordered_set<int> seen_nodes;
+  std::unordered_map<int, std::unordered_map<int, int> > seen_edges;
+  int current_pos = -1;
+  unsigned int num_links = 0, num_hashtags = 0;
+  time_t min_time = 0, max_time = 0;
 };
 
 #endif
--- src/DirectedGraph.cpp
+++ src/DirectedGraph.cpp
@@ -23,6 +23,7 @@ DirectedGraph::createSimilar() const {
   graph->setTemporal(isTemporal());
   graph->setPersonality(getPersonality());
   graph->setHasTextures(hasTextures());
+  graph->setDoubleBufferedVBO(hasDoubleBufferedVBO());
   graph->setClusterVisibility(getClusterVisibility());
   graph->setNodeVisibility(getNodeVisibility());
   graph->setEdgeVisibility(getEdgeVisibility());
@@ -32,6 +33,145 @@ DirectedGraph::createSimilar() const {
   graph->setNodeArray(nodes);
   
   return graph;
+}
+
+bool
+DirectedGraph::updateData(time_t start_time, time_t end_time, float start_sentiment, float end_sentiment, Graph & source_graph, RawStatistics & stats, bool is_first_level, Graph * base_graph) {
+  auto & sid = source_graph.getNodeArray().getTable()["source"];
+  auto & soid = source_graph.getNodeArray().getTable()["id"];
+  auto & user_type = source_graph.getNodeArray().getTable()["type"];
+  auto & political_party = source_graph.getNodeArray().getTable()["party"];
+  auto & name_column = source_graph.getNodeArray().getTable()["name"];
+  auto & uname_column = source_graph.getNodeArray().getTable()["uname"];
+
+  auto begin = source_graph.begin_edges();
+  auto end = source_graph.end_edges();
+  auto it = begin;
+  
+  if (current_pos == -1) {
+    clear();
+    edge_attributes.clear();
+    current_pos = 0;
+    cerr << "restarting update, begin = " << begin.get() << ", cp = " << current_pos << ", end = " << end.get() << ", source = " << &source_graph << ", edges = " << source_graph.getEdgeCount() << endl;
+  } else {
+    cerr << "continuing update, begin = " << begin.get() << ", cp = " << current_pos << ", end = " << end.get() << ", source = " << &source_graph << ", edges = " << source_graph.getEdgeCount() << endl;
+    for (int i = 0; i < current_pos; i++) ++it;
+  }
+
+  auto & nodes = getNodeArray();
+
+  unsigned int skipped_count = 0;
+  bool is_changed = false;
+  unsigned int num_edges_processed = 0;
+  for ( ; it != end; ++it, current_pos++) {
+    num_edges_processed++;
+
+    time_t t = 0;
+    float se = 0;
+    short lang = 0;
+    long long app_id = -1, filter_id = -1;
+    bool is_first = false;
+
+    assert(it->face != -1);
+    if (it->face != -1) {
+      auto & fd = source_graph.getFaceAttributes(it->face);
+      t = fd.timestamp;
+      se = fd.sentiment;
+      lang = fd.lang;
+      app_id = fd.app_id;
+      filter_id = fd.filter_id;
+      is_first = fd.first_edge == current_pos;
+    }
+
+    if (it->tail < 0 || it->head < 0 || it->tail >= source_graph.getNodeCount() || it->head >= source_graph.getNodeCount()) {
+      cerr << "invalid values: tail = " << it->tail << ", head = " << it->head << ", t = " << t << ", count = " << source_graph.getNodeCount() << ", n = " << num_edges_processed << endl;
+      assert(0);
+    }
+
+    if ((!start_time || t >= start_time) && (!end_time || t < end_time) &&
+	se >= start_sentiment && se <= end_sentiment) {
+      if (t < min_time || min_time == 0) min_time = t;
+      if (t > max_time) max_time = t;
+
+      pair<int, int> np(it->tail, it->head);
+      short first_user_sid = sid.getInt(np.first);
+      short target_user_sid = sid.getInt(np.second);
+      long long first_user_soid = soid.getInt64(np.first);
+      long long target_user_soid = soid.getInt64(np.second);
+      
+      if (!is_first_level) {
+	auto & td1 = base_graph->getNodeTertiaryData(np.first);
+	if (td1.indegree < getMinSignificance()) {
+	  skipped_count++;
+	  continue;
+	}
+	auto & td2 = base_graph->getNodeTertiaryData(np.second);
+	if (td2.indegree < getMinSignificance()) {
+	  skipped_count++;
+	  continue;
+	}
+      }
+
+      is_changed = true;
+
+      auto & target_nd_old = nodes.getNodeData(np.second);
+      NodeType target_type = target_nd_old.type;
+
+      if (is_first_level && is_first) {
+	stats.addActivity(t, first_user_sid, first_user_soid, lang, app_id, filter_id, PoliticalParty(political_party.getInt(np.first)));
+      }
+      
+      if (is_first_level && !seen_nodes.count(np.second)) {
+	seen_nodes.insert(np.second);
+	if (target_type == NODE_HASHTAG) {
+	  stats.addHashtag(name_column.getText(np.second));
+	  num_hashtags++;
+	} else if (target_type == NODE_URL) {
+	  stats.addLink(name_column.getText(np.second), uname_column.getText(np.second));
+	  num_links++;
+	} else {
+	  UserType ut = UserType(user_type.getInt(np.second));
+	  if (ut != UNKNOWN_TYPE) stats.addUserType(ut);
+	  // stats.addPoliticalParty(PoliticalParty(political_party.getInt(np.first)));
+	}
+      }
+
+      if (target_type != NODE_URL && target_type != NODE_HASHTAG) {
+	if (is_first_level && target_type == NODE_ANY) {
+	  stats.addReceivedActivity(t, target_user_sid, target_user_soid, app_id, filter_id);
+	}
+
+	unordered_map<int, unordered_map<int, int> >::iterator it1;
+	unordered_map<int, int>::iterator it2;
+	if ((it1 = seen_edges.find(np.first)) != seen_edges.end() &&
+	    (it2 = it1->second.find(np.second)) != it1->second.end()) {
+	  updateEdgeWeight(it2->second, 1.0f);
+	  updateOutdegree(np.first, 1.0f);
+	  if (nodes.getNodeSizeMethod().definedForSource()) {
+	    updateNodeSize(np.first);
+	  }
+	  updateIndegree(np.second, 1.0f);
+	  if (nodes.getNodeSizeMethod().definedForTarget()) {
+	    updateNodeSize(np.second);
+	  }
+	} else {
+	  seen_edges[np.first][np.second] = addEdge(np.first, np.second, -1, 1.0f);
+	}
+      }
+    }  
+  }
+
+  cerr << "updated graph data, nodes = " << getNodeCount() << ", edges = " << getEdgeCount() << ", min_sig = " << getMinSignificance() << ", skipped = " << skipped_count << ", first = " << is_first_level << endl;
+
+  if (is_first_level) {
+    stats.setTimeRange(min_time, max_time);
+    stats.setNumRawNodes(source_graph.getNodeCount());
+    stats.setNumRawEdges(source_graph.getEdgeCount());
+    // stats.setNumPosts(num_posts);
+    // stats.setNumActiveUsers(num_active_users);
+  }
+
+  return is_changed;
 }
 
 #if 0
