4285143fdcdddee199b60863b08a8470c07b5697
Mikael Rekola <mikael.rekola@gmail.com>
2016-02-17 17:53:50 +0200

    remove regions

--- include/PlanarGraph.h
+++ include/PlanarGraph.h
@@ -12,25 +12,6 @@ class PlanarGraph : public Graph {
 
   std::shared_ptr<Graph> createSimilar() const override;
   Graph * copy() const override { return new PlanarGraph(*this); }
-  
-#if 0
-  void addUniversalRegion() override;
-
-  int addRegion() override {
-    int region_id = (int)region_attributes.size();
-    region_attributes.push_back({ { 255, 255, 255, 255 }, "", Rect2d(), -1 });
-    regions.addRow();
-    return region_id;
-  }
-
-  int getRegionFirstFace(int region) const override {
-    return region_attributes[region].first_face;
-  }
-
-  int getRegionNextFace(int region, int face) const override {
-    return face_attributes[face].next_face_in_region;
-  }
-#endif
 
   std::set<int> getAdjacentRegions() const override;
 
