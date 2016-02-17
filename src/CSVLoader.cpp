1853647ad4993d75b99356a1c5aef801fb41cf60
Mikael Rekola <mikael.rekola@gmail.com>
2016-02-17 17:51:45 +0200

    switch from PointCloud to DirectedGraph, store rows as faces

--- src/CSVLoader.cpp
+++ src/CSVLoader.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <glm/glm.hpp>
 
-#include "PointCloud.h"
+#include "DirectedGraph.h"
 #include "../system/StringUtils.h"
 
 using namespace std;
@@ -16,7 +16,7 @@ CSVLoader::CSVLoader() : FileTypeHandler("Comma separated values", false) {
 
 std::shared_ptr<Graph>
 CSVLoader::openGraph(const char * filename) {
-  auto graph = std::make_shared<PointCloud>();
+  auto graph = std::make_shared<DirectedGraph>();
   graph->setNodeArray(std::make_shared<NodeArray>());
 
   ifstream in(filename, ios::in);
@@ -31,48 +31,46 @@ CSVLoader::openGraph(const char * filename) {
   cerr << "reading CSV\n";
 
   try {
-
-  while (!in.eof() && !in.fail()) {
-    string s;
-    getline(in, s);
-    StringUtils::trim(s);
-    if (s.empty()) continue;
-    vector<string> row = StringUtils::split(s, delimiter);
-    assert(!row.empty());
-
-    if (header.empty()) {
-      header = row;
-      for (vector<string>::const_iterator it = header.begin(); it != header.end(); it++) {
-	string n = StringUtils::toLower(*it);
-	if (n == "likes" || n == "count") {
-	  graph->getNodeArray().getTable().addIntColumn(it->c_str());
-	} else if (n != "x" && n != "y" && n != "z" && n != "lat" && n != "lon" &&
-		   n != "long" && n != "lng" && n != "latitude" && n != "longitude") {
-	  graph->getNodeArray().getTable().addTextColumn(it->c_str());
+    while (!in.eof() && !in.fail()) {
+      string s;
+      getline(in, s);
+      StringUtils::trim(s);
+      if (s.empty()) continue;
+      vector<string> row = StringUtils::split(s, delimiter);
+      assert(!row.empty());
+      
+      if (header.empty()) {
+	header = row;
+	for (auto it = header.begin(); it != header.end(); it++) {
+	  string n = StringUtils::toLower(*it);
+	  if (n == "likes" || n == "count") {
+	    graph->getFaceData().addIntColumn(it->c_str());
+	  } else if (n != "x" && n != "y" && n != "z" && n != "lat" && n != "lon" &&
+		     n != "long" && n != "lng" && n != "latitude" && n != "longitude") {
+	    graph->getFaceData().addTextColumn(it->c_str());
+	  }
 	}
-      }
-    } else {
-      int node_id = graph->addNode();
-      double x = 0, y = 0;
-      for (unsigned int i = 0; i < row.size(); i++) {
-	if (row[i].empty()) continue;
-	if (header[i] == "X") {
-	  x = stof(row[i]);
-	} else if (header[i] == "Y") {
-	  y = stof(row[i]);
-	} else {
-	  graph->getNodeArray().getTable()[header[i]].setValue(node_id, row[i]);	  
+      } else {
+	int face_id = graph->addFace();
+	double x = 0, y = 0;
+	for (unsigned int i = 0; i < row.size(); i++) {
+	  if (row[i].empty()) continue;
+	  if (header[i] == "X") {
+	    x = stof(row[i]);
+	  } else if (header[i] == "Y") {
+	    y = stof(row[i]);
+	  } else {
+	    graph->getFaceData()[header[i]].setValue(face_id, row[i]);	  
+	  }
 	}
+	graph->getFaceAttributes(face_id).centroid = glm::vec2(x, y);
       }
-      graph->getNodeArray().setPosition(node_id, glm::vec3(x, y, 0));
     }
-  }
-
   } catch (exception & e) {
     cerr << "exception: " << e.what() << endl;
     assert(0);
   }
-
+  
   cerr << "reading CSV done\n";
   
   return graph;
