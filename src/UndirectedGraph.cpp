25675e572e60a797614d8307d6249aca97365475
Mikael Rekola <mikael.rekola@gmail.com>
2016-02-23 11:02:34 +0200

    mirror changes in Graph and NodeArray

--- src/UndirectedGraph.cpp
+++ src/UndirectedGraph.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-UndirectedGraph::UndirectedGraph(int _id) : Graph(1, _id) {
+UndirectedGraph::UndirectedGraph(int _id) : Graph(_id) {
 }
 
 UndirectedGraph::UndirectedGraph(const UndirectedGraph & other)
@@ -14,10 +14,12 @@ std::shared_ptr<Graph>
 UndirectedGraph::createSimilar() const {
   std::shared_ptr<Graph> graph(new UndirectedGraph(getId()));
   graph->setLocationGraphValid(false);
-  graph->setTemporal(isTemporal());
-  graph->setPersonality(getPersonality());
-  graph->setHasTextures(hasTextures());
-  graph->setNodeArray(nodes);  
-
+  graph->setNodeVisibility(getNodeVisibility());
+  graph->setEdgeVisibility(getEdgeVisibility());
+  graph->setFaceVisibility(getFaceVisibility());
+  graph->setLabelVisibility(getLabelVisibility());
+  graph->setLineWidth(getLineWidth());
+  graph->setNodeArray(nodes);
+  
   return graph;
 }
