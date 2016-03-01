1ba5c008913c5b03db5719ea00609827d8c567e2
Mikael Rekola <mikael.rekola@gmail.com>
2016-03-01 21:30:00 +0200

    move iterators to separate files, implent visible node iterator

--- include/Graph.h
+++ include/Graph.h
@@ -79,64 +79,8 @@ struct face_data_s {
   const std::string & getFaceLabel(int i) const { return label; }
 };
 
-class EdgeIterator {
- public:
- EdgeIterator() : ptr(0) { }
-  EdgeIterator(edge_data_s * _ptr) : ptr(_ptr) { }
-
-  bool isValid() const { return ptr != 0; }
-
-  edge_data_s & operator*() { return *ptr; }
-  edge_data_s * operator->() { return ptr; }
-  edge_data_s * get() { return ptr; }
-
-  EdgeIterator & operator++() { ptr++; return *this; }
-  EdgeIterator & operator--() { ptr--; return *this; }
-  
-  bool operator==(const EdgeIterator & other) const { return this->ptr == other.ptr; }
-  bool operator!=(const EdgeIterator & other) const { return this->ptr != other.ptr; }
-
-  void clear() { ptr = 0; }
-
- private:
-  edge_data_s *  ptr;
-};
-
-class ConstEdgeIterator {
- public:
- ConstEdgeIterator() : ptr(0) { }
- ConstEdgeIterator( const edge_data_s * _ptr ) : ptr(_ptr) { }
-
-  bool isValid() const { return ptr != 0; }
-
-  const edge_data_s & operator*() const { return *ptr; }
-  const edge_data_s * operator->() const { return ptr; }
-  const edge_data_s * get() const { return ptr; }
-
-  ConstEdgeIterator & operator++() { ptr++; return *this; }
-  ConstEdgeIterator & operator--() { ptr++; return *this; }
-  
-  bool operator==(const ConstEdgeIterator & other) const { return this->ptr == other.ptr; }
-  bool operator!=(const ConstEdgeIterator & other) const { return this->ptr != other.ptr; }
-
-  void clear() { ptr = 0; }
-
- private:
-  const edge_data_s * ptr;
-};
-
-#if 0
-class VisibleNodeIterator {
- public:
-  VisibleNodeIterator() { }
-
- private:
-  enum Stage { LINKED_NODES = 1, PARENT_NODES, END };
-
-  Stage stage = LINKED_NODES;
-  
-};
-#endif
+#include "EdgeIterator.h"
+#include "VisibleNodeIterator.h"
 
 #include <string>
 #include <vector>
@@ -484,12 +428,8 @@ class Graph : public MBRObject {
     return it;
   }
 
-#if 0
-  ConstVisibleNodeIterator begin_visible_nodes() const { return ConstVisibleNodeIterator(); }
-  ConstVisibleNodeIterator end_visible_nodes() const {
-    
-  }
-#endif
+  ConstVisibleNodeIterator begin_visible_nodes() const { return ConstVisibleNodeIterator(&(edge_attributes.front()), &(edge_attributes.back()) + 1, &(node_geometry3.front()), nodes->size()); }
+  ConstVisibleNodeIterator end_visible_nodes() const { return ConstVisibleNodeIterator(); }
   
   int getGraphNodeId(int graph_id) const;
   bool updateSelection2(time_t start_time, time_t end_time, float start_sentiment, float end_sentiment);
