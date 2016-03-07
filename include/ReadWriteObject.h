1539696903303f6afa5a5803384c6bcd7382e2a9
Mikael Rekola <mikael.rekola@gmail.com>
2016-03-07 16:48:42 +0200

    add ReadWriteObject.h

---
+++ include/ReadWriteObject.h
@@ -0,0 +1,34 @@
+#ifndef _READWRITEOBJECT_H_
+#define _READWRITEOBJECT_H_
+
+#include "../system/Mutex.h"
+
+class ReadWriteObject {
+ public:
+  ReadWriteObject() { }
+  
+  void lockReader() const {
+    MutexLocker locker(mutex);
+    num_readers++;
+    if (num_readers == 1) writer_mutex.lock();
+  }
+  void unlockReader() const {
+    MutexLocker locker(mutex);
+    num_readers--;
+    if (num_readers == 0) {
+      writer_mutex.unlock();
+    }
+  }
+  void lockWriter() {
+    writer_mutex.lock();
+  }
+  void unlockWriter() {
+    writer_mutex.unlock();
+  }
+
+ private:
+  mutable int num_readers = 0;
+  mutable Mutex mutex, writer_mutex;
+};
+
+#endif
