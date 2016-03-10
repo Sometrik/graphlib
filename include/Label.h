cbc0e9c55e5633453ba5656e189e5fc1a04c8dc0
Mikael Rekola <mikael.rekola@gmail.com>
2016-03-10 20:21:49 +0200

    add Label.h

---
+++ include/Label.h
@@ -0,0 +1,15 @@
+#ifndef _LABEL_H_
+#define _LABEL_H_
+
+#define LABEL_FLAG_CENTER	1
+#define LABEL_FLAG_MIDDLE	2
+
+class Label {
+  glm::vec3 pos;
+  glm::vec2 offset;
+  int texture;
+  unsigned short flags;
+  glm::vec4 color1, color2;
+};
+
+#endif
