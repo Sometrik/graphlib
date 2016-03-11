2970d16dfa227c4ebc24c129321b0b01897a07d9
Mikael Rekola <mikael.rekola@gmail.com>
2016-03-11 14:27:43 +0200

    add priority column

--- include/LabelMethod.h
+++ include/LabelMethod.h
@@ -10,13 +10,15 @@ class LabelMethod {
   };
  LabelMethod(Method _method = AUTOMATIC_LABEL) : method(_method) { }
  LabelMethod(Method _method, const std::string & _column) : method(_method), column(_column) { }
+ LabelMethod(Method _method, const std::string & _column, const std::string & _priority_column) : method(_method), column(_column), priority_column(_priority_column) { }
 
   Method getValue() const { return method; }
   const std::string & getColumn() const { return column; }
+  const std::string & getPriorityColumn() const { return priority_column; }
   
  private:
   Method method;
-  std::string column;
+  std::string column, priority_column;
 };
 
 #endif
