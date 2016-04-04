0ee1d7798888cb19c173b2fa784e7ab34bc55cc0
Mikael Rekola <mikael.rekola@gmail.com>
2016-04-04 22:38:31 +0300

    make reserve abstract, remove addRow(), add abstract methods pushValue()

--- include/Column.h
+++ include/Column.h
@@ -24,11 +24,7 @@ namespace table {
 
     virtual std::shared_ptr<Column> copy() const = 0;
     virtual std::shared_ptr<Column> create() const = 0;    
-
-    void reserve(size_t n) {
-      while (size() < n) addRow();
-    }
-
+    virtual void reserve(size_t n) = 0;
     virtual size_t size() const = 0;
 
     virtual ColumnType getType() const { return TEXT; }    
@@ -43,21 +39,27 @@ namespace table {
     virtual void setValue(int i, const std::string & v) = 0;
     // virtual Column magnitude() const { return *this; }
     
-    virtual void addRow() = 0;
     virtual bool compare(int a, int b) const { return false; }
     virtual void clear() = 0;
 
+    virtual void pushValue(double v) = 0;
+    virtual void pushValue(int v) = 0;
+    virtual void pushValue(long long v) = 0;
+    virtual void pushValue(const std::string & v) = 0;
+    
     virtual Column & operator= (double a) = 0;
     virtual Column & operator= (int a) = 0;
 
     const char * getTypeText() const {
       switch (getType()) {
+      case TEXT: return "string";
       case NUMBER: return "int";
       case FLOAT: return "double";
-      case TEXT:
-      default:
-	return "string";
+      case BOOL: return "bool";
+      case TIME_SERIES: return "time_series";
+      case IMAGE_SRC: return "image_src";
       }
+      return "";
     }
     
     const std::string & name() const { return column_name; }
