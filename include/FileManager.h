4a7e3447129de1b752dd621af5b5cc49a7692b4a
Mikael Rekola <mikael.rekola@gmail.com>
2016-04-04 06:50:18 +0300

    add class FileManager

---
+++ include/FileManager.h
@@ -0,0 +1,44 @@
+#ifndef _FILEMANAGER_H_
+#define _FILEMANAGER_H_
+
+#include <string>
+#include <map>
+#include <memory>
+
+#include "FileTypeHandler.h"
+
+class Graph;
+
+#include <string>
+
+class FileManager { 
+ public:
+  static FileManager & getInstance();
+  
+  std::shared_ptr<Graph> openGraph(const std::string & filename);
+  std::shared_ptr<Graph> openGraph(const std::string & filename, const std::shared_ptr<NodeArray> & initial_nodes);
+
+  bool saveGraph(const Graph & graph, const std::string & filename);
+
+  void addHandler(std::shared_ptr<FileTypeHandler> handler);
+
+  FileTypeHandler & getHandlerByFilename(const std::string & filename);
+  FileTypeHandler & getHandlerByExtension(const std::string & extension);
+  
+  std::string getReadableFormatsString() const;
+  std::string getWritableFormatsString() const;
+
+ private:
+  FileManager();
+  ~FileManager();
+
+  static std::string getExtension(const std::string & filename);
+  
+  static FileManager * instance;
+  
+  DummyHandler null_handler;
+
+  std::map<std::string, std::shared_ptr<FileTypeHandler> > extmap;
+};
+
+#endif
---
+++ src/FileManager.cpp
@@ -0,0 +1,94 @@
+#include "FileManager.h"
+
+#include <Graph.h>
+
+using namespace std;
+
+FileManager * FileManager::instance = 0;
+
+FileManager::FileManager() {
+
+}
+
+FileManager &
+FileManager::getInstance() {
+  if (!instance) instance = new FileManager;
+  return *instance;
+}
+
+std::shared_ptr<Graph>
+FileManager::openGraph(const string & filename) {
+  return getHandlerByFilename(filename).openGraph(filename);
+}
+
+std::shared_ptr<Graph>
+FileManager::openGraph(const string & filename, const std::shared_ptr<NodeArray> & initial_nodes) {
+  return getHandlerByFilename(filename).openGraph(filename, initial_nodes);
+}
+
+bool
+FileManager::saveGraph(const Graph & graph, const string & filename) {
+  return getHandlerByFilename(filename).saveGraph(graph, filename);  
+}
+
+string
+FileManager::getExtension(const string & filename) {
+  string::size_type pos = filename.find_last_of('.');
+  if (pos == string::npos) {
+    return "";    
+  } else {
+    return filename.substr(pos + 1);
+  }
+}
+
+FileTypeHandler &
+FileManager::getHandlerByFilename(const string & filename) {
+  string extension = getExtension(filename);
+  return getHandlerByExtension(extension);
+}
+
+FileTypeHandler &
+FileManager::getHandlerByExtension(const string & extension) {
+  map<string, shared_ptr<FileTypeHandler> >::iterator it = extmap.find(extension);
+  if (it != extmap.end()) {
+    return *(it->second);
+  } else {
+    fprintf(stderr, "no handler for type \"%s\"\n", extension.c_str());
+    return null_handler;
+  }
+}
+
+void
+FileManager::addHandler(std::shared_ptr<FileTypeHandler> handler) {
+  assert(handler.get());
+  const vector<string> & extensions = handler->getExtensions();
+  for (vector<string>::const_iterator it = extensions.begin(); it != extensions.end(); it++) {
+    extmap[*it] = handler;
+  }
+}
+
+string
+FileManager::getReadableFormatsString() const {
+  string t;
+  for (map<string, std::shared_ptr<FileTypeHandler> >::const_iterator it = extmap.begin(); it != extmap.end(); it++) {
+    if (!t.empty()) t += "|";
+    t += it->second->getDescription() + " (*." + it->first + ")|*." + it->first;
+  }
+  if (!t.empty()) t+= "|";
+  t += "All files (*.*)|*.*";
+  return t;
+}
+
+string
+FileManager::getWritableFormatsString() const {
+  string t;
+  for (map<string, std::shared_ptr<FileTypeHandler> >::const_iterator it = extmap.begin(); it != extmap.end(); it++) {
+    if (it->second->canWrite()) {
+      if (!t.empty()) t += "|";
+      t += it->second->getDescription() + " (*." + it->first + ")|*." + it->first;
+    }
+  }
+  if (!t.empty()) t += "|";
+  t += "All files (*.*)|*.*";
+  return t;
+}
