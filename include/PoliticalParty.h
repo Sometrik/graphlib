9041c5fd051db15d432dfed338d8844ee3211330
Mikael Rekola <mikael.rekola@gmail.com>
2016-04-06 18:00:13 +0300

    add PoliticalParty.h

---
+++ include/PoliticalParty.h
@@ -0,0 +1,30 @@
+#ifndef _POLITICALPARTY_H_
+#define _POLITICALPARTY_H_
+
+enum PoliticalParty {
+  NO_PARTY = 0,
+
+  US_DEM,
+  US_REP,
+  
+  FI_SDP,
+  FI_VIHR,
+  FI_PIR,
+  FI_KOK,
+  FI_KESK,
+  FI_KD,
+  FI_PS,
+  FI_VAS,
+  FI_ITSP,
+  FI_M11,
+  FI_RKP,
+  FI_SKP,
+  FI_STP,
+  FI_SKS,
+  FI_KTP,
+  FI_KEMPAR,
+  FI_TUPU,
+  FI_LIB  
+};
+
+#endif
