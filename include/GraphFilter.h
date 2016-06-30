32dab04cfe36ea08941d2b4496bd6ab45ab0267d
Mikael Rekola <mikael.rekola@gmail.com>
2016-06-30 21:36:51 +0300

    add missing include, add method reset(), add method hasPosition()

--- include/GraphFilter.h
+++ include/GraphFilter.h
@@ -1,6 +1,8 @@
 #ifndef _GRAPHFILTER_H_
 #define _GRAPHFILTER_H_
 
+#include <ctime>
+
 class Graph;
 class RawStatistics;
 
@@ -10,7 +12,8 @@ class GraphFilter {
   virtual ~GraphFilter() { }
 
   virtual bool updateData(Graph & target_graph, time_t start_time, time_t end_time, float start_sentiment, float end_sentiment, Graph & source_graph, RawStatistics & stats, bool is_first_level, Graph * base_graph) = 0;
-  virtual void reset() { }  
+  virtual void reset() { }
+  virtual bool hasPosition() const { return false; }
 };
 
 #endif
