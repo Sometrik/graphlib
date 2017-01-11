7a1ed2175b5a03162eecc19e1588b675a29ff43b
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-11 22:21:29 +0200

    remove unnecessary code

--- include/FileTypeHandler.h
+++ include/FileTypeHandler.h
@@ -3,23 +3,22 @@
 
 #include <vector>
 #include <string>
-#include <map>
 #include <memory>
 
-class ArcData2D;
 class Graph;
 class NodeArray;
 
 class FileTypeHandler {
  public:
-  FileTypeHandler(const std::string & _description, bool can_write);
+  FileTypeHandler(const std::string & _description, bool _can_write)
+    : description(_description), can_write(_can_write) {
+    
+  }
   virtual ~FileTypeHandler() { }
   
   virtual std::shared_ptr<Graph> openGraph(const char * filename, const std::shared_ptr<NodeArray> & initial_nodes) = 0;
   
   std::shared_ptr<Graph> openGraph(const std::string & filename, const std::shared_ptr<NodeArray> & initial_nodes) { return openGraph(filename.c_str(), initial_nodes); }
-
-  std::shared_ptr<Graph> openGraph(const std::string & filename);
 
   virtual bool saveGraph(const Graph & graph, const std::string & filename) { return false; }
   
@@ -28,9 +27,6 @@ class FileTypeHandler {
   bool canWrite() const { return can_write; }
 
  protected:
-  static int createNode2D(Graph & graph, std::map<std::string, int> & nodes, double x, double y);
-  static int createNode3D(Graph & graph, std::map<std::string, int> & nodes, double x, double y, double z = 0);
-  static std::pair<int, int> createNodesForArc(const ArcData2D & arc, Graph & graph, std::map<std::string, int> & nodes, bool rev = false);
 
   void addExtension(const std::string & str) { extensions.push_back(str); }
   
